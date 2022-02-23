45a074fc5437c435d65e38f590c7f6f595f2e543
Antonio Castillo <acrocamora@gmail.com>
2022-02-23 09:50:34 +0100

    edit app.js

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const {connect} = require('mongoose');
+const {MONGODB_URL} = process.env;
+connect(MONGODB_URL, () => console.log('base de datos conectada'));
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/drinks', authorization, tasksRouter);
+app.use('/authors', authorsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
